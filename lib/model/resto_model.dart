58aa93649c3148a370ddb03a7fd08bd22a1732de
Revaldo Putra <revaldoputra11@gmail.com>
2022-05-11 21:45:12 +0700

    remove comment code

--- lib/model/resto_model.dart
+++ lib/model/resto_model.dart
@@ -1,37 +1,3 @@
-// import 'dart:ffi';
-
-// class RestoModel {
-//   late int id;
-//   late String name;
-//   late String description;
-//   late String pictureId;
-//   late String city;
-//   late Double rating;
-//   // late String menus;
-//   // late List<String> foods;
-//   RestoModel({
-//     required this.id,
-//     required this.name,
-//     required this.description,
-//     required this.pictureId,
-//     required this.city,
-//     required this.rating,
-//     // required this.menus,
-//     // required this.foods,
-//   });
-
-//   static RestoModel fromJson(json) => RestoModel(
-//         id: json['id'],
-//         name: json['name'],
-//         description: json['description'],
-//         pictureId: json['pictureId'],
-//         city: json['city'],
-//         rating: json['rating'],
-//         // menus: json['menus'],
-//         // foods: json['foods'],
-//       );
-// }
-
 class RestoModel {
   //data Type
   late String id;
